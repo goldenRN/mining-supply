44728ef7c70bd9f2db2388ba0e927c63f0b1976b
altantuya bereevenjav <altantuyabereevenjav@MacBook-Pro.local>
2025-11-07 11:13:25 +0800

    dashboard admin zassan

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,20 +1,15 @@
-
 'use client'
+
 import { usePathname } from 'next/navigation'
 
-export default function AdminPage({ children }: { children: React.ReactNode }) {
+export default function AdminDashboardPage() {
     const pathname = usePathname()
-    return (
 
+    return (
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="bg-muted/50 aspect-video rounded-xl" />
             <div className="bg-muted/50 aspect-video rounded-xl" />
             <div className="bg-muted/50 aspect-video rounded-xl" />
         </div>
-        // <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" /> 
-
     )
 }
-
-
-
